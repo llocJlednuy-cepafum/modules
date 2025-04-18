e2e154af2c32f4e2cb9e442a2ae22ff8d005897d
╭(￣▽￣)╯ ʟʟᴏᴄᴊʟᴇᴅɴᴜʏ × ᴄᴇᴘᴀғᴜᴍ - ᴍᴏᴅᴜʟᴇ's ╧═╧ <mail.com.from.p@gmail.com>
2025-04-18 21:27:15 +0300

    Add files via upload

--- CheckUsername.py
+++ CheckUsername.py
@@ -1,20 +1,20 @@
 __version__ = (1, 1, 4)
 
-# -------------------------------------------------------------------------------- 
-#                                                                                  
-# Name: CheckUsername                                                            
-# Description: пᴘовᴇᴘкᴀ имя пользовᴀтᴇля в ᴛᴇʟᴇɢʀᴀᴍ                          
-# meta developer: @ManulMods                                             
-# authors: @ManulMods
-# version: 1.1.4                                                                                 
+# --------------------------------------------------------------------------------
+#
+# Name: CheckUsername
+# Description: пᴘовᴇᴘкᴀ имя пользовᴀтᴇля в ᴛᴇʟᴇɢʀᴀᴍ
+# meta developer: @ManulMods
+# authors: @llocJlednuy_cepafum, @ManulMods
+# version: 1.1.4
 #
 # █▀▀ █▀▀ █▀█ ▄▀▄ █▀▀ █ █ █▄ ▄█
 # █▄▄ ██▄ █▀▀ █▀█ █▀  █▄█ █ ▀ █
 #
-# ▀▄▀ █▄ ▄█ █▄ ▄█ █▀█ █▀▄ █ █ █   █▀▀ █▀▀ 
-#  █  █ ▀ █ █ ▀ █ █▄█ █▄▀ █▄█ █▄▄ ██▄ ▄██ 
+# ▀▄▀ █▄ ▄█ █▄ ▄█ █▀█ █▀▄ █ █ █   █▀▀ █▀▀
+#  █  █ ▀ █ █ ▀ █ █▄█ █▄▀ █▄█ █▄▄ ██▄ ▄██
 #
-# -------------------------------------------------------------------------------- 
+# --------------------------------------------------------------------------------
 
 from telethon.tl import functions
 import logging
@@ -41,27 +41,30 @@ class CheckUsername(loader.Module):
         self._db = db
         self._client = client
 
-    
     @loader.command()
     async def check(self, message):
         """<юзернейм> - пᴘовᴇᴘяᴇт достʏпность имᴇни пользовᴀтᴇля"""
         args = utils.get_args_raw(message)
+
         if not args:
             await utils.answer(message, self.strings["check_false_args"])
             return
-        
+
         username = args.strip()
 
-        result = await self.check_username_availability(self._client, username)
+        result = await self.check_username_availability(username)
 
         if result:
             await utils.answer(message, self.strings["check_false"].format(username))
         else:
             await utils.answer(message, self.strings["check_true"].format(username))
-    
-    async def check_username_availability(self, message, username: str) -> bool:
+
+    async def check_username_availability(self, username: str) -> bool:
+        try:
             request = functions.account.CheckUsernameRequest(username=username)
             result = await self._client(request)
             return result
-
+        except Exception as e:
+            logging.exception(f"Ошибка при проверке юзернейма {username}: {e}")
+            return False
         
